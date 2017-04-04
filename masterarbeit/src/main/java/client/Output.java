67e81d43b2b4a7313ee8420a4684b02d5a0a7516
tim kuerten <tim_kurten@intuit.com>
2017-04-04 15:20:38 +0200

    Output interface

--- masterarbeit/src/main/java/client/Output.java
+++ masterarbeit/src/main/java/client/Output.java
@@ -1,7 +1,32 @@
 package client;
 
+import datastructure.Profile;
+import datastructure.Schema;
+import datastructure.Triple;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
 public interface Output {
 
 
+    void get(UUID uuid, Profile returnValue);
+
+    void get(String thirdPartyID, String value, Set<Profile> returnValue);
+
+    void get(String thirdPartyID, String minValue, String maxValue, Set<Profile> returnValue);
+
+    void get(Set<Triple<Integer, String, String>> searchValues, Set<Profile> returnValue);
+
+    void insert(Map<String, String> profileData, UUID returnValue);
+
+    void update(UUID uuid, Map<String, String> profileData, boolean returnValue);
+
+    void getSchema(Schema returnValue);
+
+    void changeSchema(Set<String> schema, Set<String> thirdPartyIDs, boolean returnValue);
+
+    void addSchema(Set<String> schema, Set<String> thirdPartyIDs, boolean returnValue);
 
 }
